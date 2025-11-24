c09535fa2c0244e69bad53f2bf71c6f388109417
Wei Yang <wei0038@proton.me>
2025-11-24 11:42:09 +0100

    added models objects

---
+++ Pokedex-api/src/models/Deck.ts
@@ -0,0 +1,34 @@
+export class Deck {
+  #id: number;
+  #name: string;
+  #characterId: number;
+
+  constructor(
+    id: number,
+    name: string,
+    characterId: number,
+  ) {
+    this.#id = id;
+    this.#name = name;
+    this.#characterId = characterId;
+  }
+
+  get id(): number {
+    return this.#id;
+  }
+
+  get name(): string {
+    return this.#name;
+  }
+
+  get characterId(): number {
+    return this.#characterId;
+  }
+
+  set name(value: string) {
+    if (value.length === 0) {
+      throw new Error("Deck name cannot be empty");
+    }
+    this.#name = value;
+  }
+}
