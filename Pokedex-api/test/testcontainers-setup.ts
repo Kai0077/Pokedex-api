706b936553c5f8ccc2cc2c159c6a40f4047cfae0
Wei Yang <wei0038@proton.me>
2025-12-03 16:58:40 +0100

    fix: pipeline

--- Pokedex-api/test/testcontainers-setup.ts
+++ Pokedex-api/test/testcontainers-setup.ts
@@ -5,7 +5,6 @@ export default async function () {
   const initSql = fs.readFileSync("test/init.sql", "utf8");
 
   const container = await new MySqlContainer("mysql:8.0")
-    .withTmpFs({ "/var/lib/mysql": "rw" })
     .withEnvironment({
       MYSQL_ROOT_PASSWORD: "3a!k$YD@$",
       MYSQL_DATABASE: "pokedex_test",
@@ -16,7 +15,6 @@ export default async function () {
         target: "/docker-entrypoint-initdb.d/init.sql",
       },
     ])
-    .withCommand(["--default-authentication-plugin=mysql_native_password"])
     .start();
 
   process.env.DB_HOST = container.getHost();
@@ -24,13 +22,6 @@ export default async function () {
   process.env.DB_USER = "pokedex_user_test";
   process.env.DB_PASSWORD = "3a!k$YD@$";
   process.env.DB_NAME = "pokedex_test";
-
-  console.log("### Testcontainers env applied:", {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    name: process.env.DB_NAME,
-  });
 
   return async () => container.stop();
 }
