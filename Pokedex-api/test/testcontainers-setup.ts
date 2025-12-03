182740dd3b14cde0c7d309e8a89eb4f65fb4f4e3
Wei Yang <wei0038@proton.me>
2025-12-03 16:28:30 +0100

    fix: some

--- Pokedex-api/test/testcontainers-setup.ts
+++ Pokedex-api/test/testcontainers-setup.ts
@@ -1,17 +1,29 @@
 import { MySqlContainer } from "@testcontainers/mysql";
+import fs from "fs";
 
 export default async function () {
+  const initSql = fs.readFileSync("test/init.sql", "utf8");
+
   const container = await new MySqlContainer("mysql:8.0")
-    .withDatabase("pokedex_test")
-    .withUsername("pokedex_user_test")
-    .withRootPassword("3a!k$YD@$")
+    .withTmpFs({ "/var/lib/mysql": "rw" })
+    .withEnvironment({
+      MYSQL_ROOT_PASSWORD: "3a!k$YD@$",
+      MYSQL_DATABASE: "pokedex_test",
+    })
+    .withCopyContentToContainer([
+      {
+        content: initSql,
+        target: "/docker-entrypoint-initdb.d/init.sql",
+      },
+    ])
+    .withCommand(["--default-authentication-plugin=mysql_native_password"])
     .start();
 
   process.env.DB_HOST = container.getHost();
-  process.env.DB_PORT = container.getMappedPort(3306).toString();
-  process.env.DB_USER = container.getUsername();
-  process.env.DB_PASSWORD = container.getRootPassword();
-  process.env.DB_NAME = container.getDatabase();
+  process.env.DB_PORT = container.getPort().toString();
+  process.env.DB_USER = "pokedex_user_test";
+  process.env.DB_PASSWORD = "3a!k$YD@$";
+  process.env.DB_NAME = "pokedex_test";
 
   console.log("### Testcontainers env applied:", {
     host: process.env.DB_HOST,
@@ -20,7 +32,5 @@ export default async function () {
     name: process.env.DB_NAME,
   });
 
-  return async () => {
-    await container.stop();
-  };
+  return async () => container.stop();
 }
